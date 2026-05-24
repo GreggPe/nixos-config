fd24cd0edf4ac11ed4959a94f047c2efeba7acda
GreggPe <gregoryperisset06@proton.me>
2026-05-24 23:15:49 +0200

    Theme: dark mode global (prefer-dark + GTK Adwaita-dark)

--- modules/users/gregory/_desktop.nix
+++ modules/users/gregory/_desktop.nix
@@ -1,9 +1,29 @@
-{ ... }:
+{ pkgs, ... }:
 {
   # Pose la config Niri au bon endroit
   xdg.configFile."niri/config.kdl".source = ./_niri.kdl;
 
   programs.fastfetch.enable = true;
+
+  # Préférence sombre standardisée (Nautilus + apps modernes)
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
+
+  # Thème GTK sombre (renfort)
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome-themes-extra;
+    };
+    iconTheme = {
+      name = "Adwaita";
+      package = pkgs.adwaita-icon-theme;
+    };
+  };
   # tmux (ton multiplexeur, le vrai outil pentest)
   programs.tmux = {
     enable = true;
